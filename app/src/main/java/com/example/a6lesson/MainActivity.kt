34a41459e6ab64bd035cba2235c8fa3e2f06617d
sssevil <zhunushova29@gmail.com>
2020-07-08 01:52:19 +0600

    third commit

--- app/src/main/java/com/example/a6lesson/MainActivity.kt
+++ app/src/main/java/com/example/a6lesson/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.a6lesson
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var tvTitle: TextView?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvTitle = findViewById(R.id.tvTitle)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       val intent = Intent(applicationContext,prodact:: class.java)
+        val intent1 = Intent(applicationContext,service:: class.java)
+        when(item.itemId) {
+            R.id.product -> startActivity(intent)
+            R.id.service -> startActivity(intent1)
+            R.id.application -> tvTitle?.text=getString(R.string.application)
+            R.id.reviews-> tvTitle?.text= getString(R.string.reviews)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
