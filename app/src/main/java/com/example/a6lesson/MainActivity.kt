23d7de5737727945470e367d6e2f79fdab7a57f1
sssevil <zhunushova29@gmail.com>
2020-07-07 15:38:38 +0600

    first commit

---
+++ app/src/main/java/com/example/a6lesson/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.a6lesson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
